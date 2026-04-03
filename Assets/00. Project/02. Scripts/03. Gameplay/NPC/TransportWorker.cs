7bdccf8f3c8e93fcc8e09ed58aafb427cd2f3939
TaeGun08 <127374475+TaeGun08@users.noreply.github.com>
2026-04-04 07:02:31 +0900

    버그 수정 중

--- Assets/00. Project/02. Scripts/03. Gameplay/NPC/TransportWorker.cs
+++ Assets/00. Project/02. Scripts/03. Gameplay/NPC/TransportWorker.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 
 public enum TransportState { ToCauldron, WaitingAtCauldron, ToTable, UnloadingAtTable, Distributing }
@@ -16,11 +17,12 @@ public class TransportWorker : Npc
     private MagicCauldron targetCauldron;
     private PotionTable targetTable;
     private TransportState currentState;
-    private WaitForSeconds wait = new WaitForSeconds(0.3f);
+    private WaitForSeconds wait;
 
     protected override void Awake()
     {
         base.Awake();
+        wait = new WaitForSeconds(0.3f);
         targetCauldron = FindObjectOfType<MagicCauldron>();
         targetTable = FindObjectOfType<PotionTable>();
     }
@@ -35,7 +37,6 @@ public class TransportWorker : Npc
     {
         while (true)
         {
-            // 1. 가마솥으로 이동 및 완충 대기
             currentState = TransportState.ToCauldron;
             MoveTo(cauldronPoint.position);
             yield return new WaitUntil(() => !IsMoving);
@@ -43,7 +44,6 @@ public class TransportWorker : Npc
             currentState = TransportState.WaitingAtCauldron;
             ItemStacker cauldronOutput = targetCauldron.GetOutputStacker();
             
-            // [Strict Loading]: 내 스택이 꽉 찰 때까지 무한 대기
             while (!myStacker.IsFull)
             {
                 if (cauldronOutput.HasItem)
@@ -57,12 +57,12 @@ public class TransportWorker : Npc
                 yield return wait;
             }
 
-            // 2. 테이블로 이동
             currentState = TransportState.ToTable;
             MoveTo(tablePoint.position);
             yield return new WaitUntil(() => !IsMoving);
+            
+            transform.DORotate(new Vector3(0, 180f, 0), 0.3f);
 
-            // 3. 하적 (내 스택이 완전히 비워질 때까지)
             currentState = TransportState.UnloadingAtTable;
             ItemStacker tableInput = targetTable.GetStacker();
             while (myStacker.HasItem)
@@ -78,22 +78,20 @@ public class TransportWorker : Npc
                 yield return wait;
             }
 
-            // 4. 테이블 검사 및 배급 결정
             if (tableInput.HasItem)
             {
-                // [Distribution Phase]: 테이블에 포션이 있는 동안 상주하며 배급
                 currentState = TransportState.Distributing;
                 MoveTo(distributionStandPoint.position);
                 yield return new WaitUntil(() => !IsMoving);
+                
+                transform.DORotate(new Vector3(0, 180f, 0), 0.3f);
 
-                // 테이블의 포션이 다 떨어질 때까지 대기 (배급 지원)
                 while (tableInput.HasItem)
                 {
                     yield return wait;
                 }
             }
             
-            // 배급 완료 혹은 테이블이 비어있으면 다시 가마솥으로 (Loop)
             yield return null;
         }
     }
