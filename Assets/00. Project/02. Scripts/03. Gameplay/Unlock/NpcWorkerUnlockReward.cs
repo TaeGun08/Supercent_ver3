cdea3cca07b840e36a5ce1958ac2a00cb97c7abc
TaeGun08 <gustn4984@naver.com>
2026-04-05 03:34:09 +0900

    이펙트 추가 끝

--- Assets/00. Project/02. Scripts/03. Gameplay/Unlock/NpcWorkerUnlockReward.cs
+++ Assets/00. Project/02. Scripts/03. Gameplay/Unlock/NpcWorkerUnlockReward.cs
@@ -6,7 +6,8 @@ public class NpcWorkerUnlockReward : MonoBehaviour, IUnlockReward
 {
     [Header("Reward Settings")]
     [SerializeField] private List<GameObject> workersToActivate;
-
+    [SerializeField] private GameObject nextUnlock;
+    
     private void Awake()
     {
         if (workersToActivate != null)
@@ -23,12 +24,10 @@ public class NpcWorkerUnlockReward : MonoBehaviour, IUnlockReward
         if (workersToActivate == null) return;
         foreach (var worker in workersToActivate)
         {
-            if (worker != null)
-            {
-                worker.SetActive(true);
-                // 인부 활성화 이펙트 재생 (인부 위치)
-                EffectManager.Instance.PlayEffect("WorkerUnlock", worker.transform.position);
-            }
+            if (worker == null) continue;
+            worker.SetActive(true);
+            EffectManager.Instance.PlayEffect("WorkerUnlock", worker.transform.position);
         }
+        nextUnlock?.gameObject.SetActive(true);
     }
 }
